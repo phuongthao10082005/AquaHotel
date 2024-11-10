ee62cf296d26ce6d884aeb63017e9f606d1c831b
phuongthao10082005 <thaontpph50562@gmail.com>
2024-11-10 23:34:29 +0700

    register

---
+++ register.php
@@ -0,0 +1,170 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Pearl ::<?php echo "login"; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!DOCTYPE HTML>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Pearl :: <?php echo $title; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="icon" href="images/d1.png" sizes="32x32" />
+        <link rel="icon" href="images/d1.png" sizes="192x192" />
+        <link rel="apple-touch-icon-precomposed" href="images/d1.png" />
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/prettyPhoto.css">
+        <link rel="stylesheet" href="css/w3.css">
+        <link rel="stylesheet" href="css/smoothness/jquery-ui-1.10.4.custom.min.css">
+        <link rel="stylesheet" href="rs-plugin/css/settings.css">
+        <link rel="stylesheet" href="css/theme.css">
+        <link rel="stylesheet" href="css/colors/turquoise.css" id="switch_style">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600,700">
+
+        <!-- Javascripts -->
+        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.nicescroll.js"></script>
+        <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
+        <script type="text/javascript" src="js/jquery.forms.js"></script>
+        <script type="text/javascript" src="js/jquery.sticky.js"></script>
+        <script type="text/javascript" src="js/waypoints.min.js"></script>
+        <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+        <script type="text/javascript" src="js/jquery.gmap.min.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="js/switch.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+
+<body>
+
+    <!-- Top header -->
+    <div id="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6">
+                    <div class="th-text pull-left">
+                        <div class="th-item" id="koko"> <a href="#"><i class="fa fa-phone"></i> +91-1201112223 +91-1201112333</a> </div>
+                        <div class="th-item"> <a href="#"><i class="fa fa-envelope"></i>hotelbooking@gmail.com </a></div>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="th-text pull-right">
+                        <div class="th-item">
+
+                        </div>
+                        <div class="th-item">
+                            <div class="social-icons"> <a href="" target="_blank"><i class="fa fa-facebook"></i></a> <a href="" target="_blank"><i class="fa fa-twitter"></i> <a href="target=" _blank"><i class="fa fa-youtube"></i></a><a href="target=" _blank"><i class="fa fa-google"></i></a><a href="" target="_blank"><i class="fa fa-gmail"></i></a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <header>
+        <!-- Navigation -->
+        <div class="navbar yamm navbar-default" id="sticky">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                    <a href="../index.php" class="navbar-brand">
+                        <!-- Logo -->
+                        <div id="logo" style="border:none"> <img id="default-logo" src="images/d1.png" alt="Darshnam" style="width:50%;height:100%;"> </div>
+                    </a>
+                </div>
+                <div id="navbar-collapse-grid" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown"> <a href="../index.php">Home</a>
+                        </li>
+
+                        <li> <a href="../gallery.php">Gallery</a></li>
+
+
+
+
+                        <li><a href="../contact-02.php">Contact</a></li>
+                        <li><a href="#">Rooms</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="parallax-effect">
+        <div id="parallax-pagetitle" style="background-image: url(./images/parallax/parallax-01.jpg);">
+            <div class="color-overlay">
+                <!-- Page title -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <ol class="breadcrumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">SIGN-UP</li>
+                            </ol>
+                            <h1>SIGN-UP</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid">
+        <div class="row" style="margin-top: 20px">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <h2 class="lined-heading"><span>SIGN-UP</span></h2>
+                <form action="processsignup.php" class="form-group" method="post">
+                    <label>First Name</label>
+                    <input type="text" name="firstname" placeholder="Enter your first name" class="form-control" required>
+                    <br>
+                    <label>Last Name</label>
+                    <input type="text" name="lastname" placeholder="Enter your last name" class="form-control" required>
+                    <br>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Enter your email" class="form-control" required>
+                    <br>
+                    <label>Phone Number</label>
+                    <input type="tel" name="phone" placeholder="Enter your phone number" class="form-control" required>
+                    <br>
+                    <label>National ID</label>
+                    <input type="text" name="nationalid" placeholder="Enter your national ID" class="form-control" required>
+                    <br>
+                    <label>Birthday</label>
+                    <input type="date" name="birthday" class="form-control" required>
+                    <br>
+                    <input type="submit" class="btn btn-primary form-control" value="Sign Up">
+                </form>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+
+    <?php include("../footer.php"); ?>
+</body>
+
+</html>
